a65200e641a4d24cdf3ac9724fd2e4e1e60378f6
Voncloft <voncloft@gmail.com>
2021-04-27 12:01:26 -0400

    Minor Updates

---
+++ Chapter_6/18-attr.sh
@@ -0,0 +1,26 @@
+name=attr
+version=2.4.48
+
+mkdir -pv /var/lib/scratchpkg/index/$name
+checkfile=/var/lib/scratchpkg/index/$name/.pkginfo
+
+if [ ! -f $checkfile ];
+then
+tar xvf $name-$version.tar.xz
+cd $name-$version
+./configure --prefix=/usr     \
+            --bindir=/bin     \
+            --disable-static  \
+            --sysconfdir=/etc \
+            --docdir=/usr/share/doc/attr-$version
+            make
+            make install
+            mv -v /usr/lib/libattr.so.* /lib
+	ln -sfv ../../lib/$(readlink /usr/lib/libattr.so) /usr/lib/libattr.so
+rm -rvf $name-$version
+
+#add record to system install library
+echo "name=$name" >> $checkfile
+echo "version=$version" >> $checkfile
+echo "release=1" >> $checkfile
+fi
