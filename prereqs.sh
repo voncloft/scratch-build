f98039303e9121ef6970111bdb6f4d84775f51d5
Voncloft <voncloft@gmail.com>
2021-04-27 23:39:10 -0400

    Minor Updates

--- prereqs.sh
+++ prereqs.sh
@@ -17,11 +17,20 @@ chmod -v a+wt $LFS/sources
 wget --input-file=files/wget-list --continue --directory-prefix=$LFS/sources
 
 echo "Creating filesystem"
-mkdir -pv $LFS/{bin,etc,lib,sbin,usr,var}
+#mkdir -pv $LFS/{bin,etc,lib,sbin,usr,var}
+mkdir -pv $LFS/bin
+mkdir -pv $LFS/etc
+mkdir -pv $LFS/lib
+mkdir -pv $LFS/sbin
+mkdir -pv $LFS/usr
+mkdir -pv $LFS/var
+
 case $(uname -m) in
   x86_64) mkdir -pv $LFS/lib64 ;;
 esac
-mkdir -pv $LFS/lib{,x}32
+mkdir -pv $LFS/lib32
+mkdir -pv $LFS/libx32
+
 mkdir -pv $LFS/tools
 
 echo "Creating lfs user"
@@ -30,11 +39,18 @@ useradd -s /bin/bash -g lfs -m -k /dev/null lfs
 
 ###Find way to passwd lfs
 #passwd lfs
-chown -v lfs $LFS/{usr,lib,var,etc,bin,sbin,tools}
+chown -v lfs $LFS/usr
+chown -v lfs $LFS/lib
+chown -v lfs $LFS/var
+chown -v lfs $LFS/etc
+chown -v lfs $LFS/bin
+chown -v lfs $LFS/sbin
+chown -v lfs $LFS/tools
 case $(uname -m) in
   x86_64) chown -v lfs $LFS/lib64 ;;
 esac
-chown -v lfs $LFS/{lib32,libx32}
+chown -v lfs $LFS/lib32
+chown -v lfs $LFS/libx32
 
 mkdir -pv /logs
 chown -v lfs /logs
